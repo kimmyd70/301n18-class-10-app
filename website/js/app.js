dc13f47ded90a0d8fdd077bcf584c0b5c072892a
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2020-06-30 21:13:49 -0700

    update with ajax

--- website/js/app.js
+++ website/js/app.js
@@ -4,18 +4,23 @@ function renderThings(list) {
   const template = $('#thingsToDoTemplate').html();
   const container = $('#things');
   list.forEach(item => {
-    let newItemHTML = Mustache.render(template, item)
+    let newItemHTML = Mustache.render(template,item)
     container.append(newItemHTML);
   });
 }
 
 function showThingsToDo() {
 
-  let things = [
-    { task: 'watch tv' },
-    { task: 'take a nap' },
-  ];
-  renderThings(things);
+  //ajax call here:
+  $.ajax('http://localhost3000/todo')
+    .then (stuff => {
+      renderThings(stuff)
+    });
+
+  // let things = [
+  //   { task: 'watch tv' },
+  //   { task: 'take a nap' },
+  // ];
 
 }
 
